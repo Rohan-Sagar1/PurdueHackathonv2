dc5eee3729835177876814dc3322e8798c7dbb79
Rohan Sagar <sagarrohan2002@gmail.com>
2023-01-21 17:57:17 -0500

    update

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
