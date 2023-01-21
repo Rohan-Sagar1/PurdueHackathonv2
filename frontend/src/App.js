e631bf3737ad16a2c318ef0a916e098d5163d275
Rohan Sagar <sagarrohan2002@gmail.com>
2023-01-21 18:05:02 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Sidebar />
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
