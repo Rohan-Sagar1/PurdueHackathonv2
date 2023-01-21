8569fd24380906edb7e668640cf36ed88f83af41
Rohan Sagar <sagarrohan2002@gmail.com>
2023-01-21 17:59:30 -0500

    Update Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -3,8 +3,22 @@ import styled from "styled-components";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <Nav>
+    
+    </Nav>
   )
 }
 
 export default Sidebar
+
+const Nav = styled.div`
+    width: 260px;
+    height: 100vh;
+    background-color: #f8f9fa;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 999;
+    border-right: 1px solid #ddd;
+`;
