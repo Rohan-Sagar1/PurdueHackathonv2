19b2399ed725942430dfcaa6be41de264d33a521
kevintl1998 <kevintl1998@gmail.com>
2023-01-21 19:25:23 -0500

    ggg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Sidebar />
+      <Header />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import styled from 'styled-components';
+
+
+const Top = styled.div`
+    display : flex;
+    flex-direction: row;
+    width: 80%;
+    height: 10vh;
+    background-color: #fdfdd2;
+    position: fixed;
+    top: 0;
+    left: 20%;
+    bottom: 0;
+    z-index: 999;
+    border-bottom: 1px solid #ddd;
+    border-left: 1px solid #ddd;
+    vertical-align:  middle;
+`;
+
+const Search = styled.input.attrs({
+  type:"text",
+  id:"search",
+  placeholder: "Search for ads..",
+})`
+position: absolute;
+left: 5%;
+top: 50%;
+transform: translateY(-50%);
+`;
+
+const User = styled.div`
+  position: absolute;
+  left: 83%;
+  top: 50%;
+  transform: translateY(-50%);  
+  background-color: #FFFFFF;
+  width: 15%;
+  height: 7vh;
+`;
 
 function Header() {
   return (
-    <div>Header</div>
+    <Top>
+      <Search></Search>
+      <User>
+        <h3>Saheer Ahmad</h3>
+      </User>
+    </Top>
+      
   )
 }
 
-export default Header
+export default Header;
