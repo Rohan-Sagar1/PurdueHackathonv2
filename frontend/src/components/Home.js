9fe206f0e7ef886507bd8931b192d8853a53fb32
Rohan Sagar <sagarrohan2002@gmail.com>
2023-01-21 20:43:41 -0500

    Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,16 +2,18 @@ import * as React from 'react'
 import styled from 'styled-components'
 import Card from './Card'
 import Dashboard from './Dashboard'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// when href is /jobs, render the Dashboard component
 
 function Home() {
-  return (
-    <Container>
-        <Dashboard />
-        
-
-
-    </Container>
-  )
+    return (
+        <Container>
+            <Routes>
+                <Route path="/jobs" element={<Dashboard />} />
+            </Routes>
+        </Container>
+    )
 }
 
 export default Home
