3d2fb24d49f6663f61dbb13d6fd95f2d94110571
Rohan Sagar <sagarrohan2002@gmail.com>
2023-01-21 21:05:14 -0500

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
 import Home from './components/Home';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
-  return (
-    <Router>
-      <Sidebar />
-      <Header />
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
-      </Routes>
-    </Router>
-  );
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={
+          <>
+            <Header />
+            <Home />
+            <Sidebar />
+          </>
+      } />
+        </Routes>
+        </Router>
+    )
 }
 
 export default App;
